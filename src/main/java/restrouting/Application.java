3d6072772539f9d60107f3d41a6db05a843ecb51
FxKu <felix-kunde@gmx.de>
2016-01-15 15:22:38 +0100

    enable API for Swagger

--- src/main/java/restrouting/Application.java
+++ src/main/java/restrouting/Application.java
@@ -2,11 +2,46 @@ package restrouting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackageClasses = {
+	    RoutingController.class
+	})
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public Docket routingApi() { 
+        return new Docket(DocumentationType.SWAGGER_2)
+          .groupName("excell-routing-api")
+          .select()                                  
+          	//.apis(RequestHandlerSelectors.any())              
+          	//.paths(PathSelectors.any())                          
+          .build()
+          .apiInfo(apiInfo())
+          ;
+    }
+    
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+          "ExCELL Routing API",
+          "This API provides an optimized routing for the ExCELL test area (Dresden).",
+          "Version 1.0",
+          "Use only for testing",
+          "fkunde@beuth-hochschule",
+          "Apache 2",
+          "http://www.apache.org/licenses/LICENSE-2.0");
+        return apiInfo;
     }
 }
--- src/main/java/restrouting/RoutingController.java
+++ src/main/java/restrouting/RoutingController.java
@@ -3,15 +3,18 @@ package restrouting;
 import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
 import routingapi.RoutingService;
 
 @RestController
 public class RoutingController {	
 	
-    @RequestMapping("/routing")
+    @RequestMapping(value = "/v1/routing", method = RequestMethod.GET)
+    @ApiOperation(value = "routing", nickname = "startRouting", produces = "application/json")
     public JSONObject routing(@RequestParam(value="startLat", defaultValue="0.0") Double startLat,
     		@RequestParam(value="startLon", defaultValue="0.0") Double startLon,
     		@RequestParam(value="endLat", defaultValue="0.0") Double endLat,
