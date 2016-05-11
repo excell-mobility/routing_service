530e81f1ce82963957d98826c2f16a52def4487d
Pieper, Stephan <spieper@beuth-hochschule.de>
2016-05-11 10:04:26 +0200

    Changed back to HTTP

--- src/main/java/restrouting/Application.java
+++ src/main/java/restrouting/Application.java
@@ -37,7 +37,7 @@ public class Application {
           	//.paths(PathSelectors.any())
           .build()
           .genericModelSubstitutes(ResponseEntity.class)
-          .protocols(Sets.newHashSet("https"))
+//          .protocols(Sets.newHashSet("https"))
           .host("dbl43.beuth-hochschule.de/excell-routing-api")
           .apiInfo(apiInfo())
           ;
