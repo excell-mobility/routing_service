efecd4f91f0bc5fd9eb2c969bb902ddaafd2ce1f
Manal <s57638@beuth-hochschule.de>
2016-06-30 17:47:40 +0200

    - reformed graph - fixed exp

--- src/main/java/restrouting/extended_gh/test_gh/GraphBuilding.java
+++ src/main/java/restrouting/extended_gh/test_gh/GraphBuilding.java
@@ -1,14 +1,8 @@
 package restrouting.extended_gh.test_gh;
 
 import com.graphhopper.*;
-import com.graphhopper.routing.QueryGraph;
 import com.graphhopper.routing.util.*;
 import com.graphhopper.storage.*;
-import com.graphhopper.storage.index.LocationIndex;
-import com.graphhopper.storage.index.LocationIndexTree;
-import com.graphhopper.storage.index.QueryResult;
-import com.graphhopper.util.EdgeIteratorState;
-import com.graphhopper.util.Helper;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -20,81 +14,81 @@ import static org.junit.Assert.assertTrue;
 public class GraphBuilding {
     protected static String location = "D:\\Arbeit\\workspace\\restroutinghopper\\src\\main\\resources\\graphhopper";
 
-    private static EncodingManager encodingManager = new EncodingManager("CAR");
-    private static GraphHopperStorage graph;
-//    private static LocationIndex index;
+    //private static EncodingManager encodingManager = new EncodingManager("CAR");
+    //private static GraphHopperStorage graph;
+    private static CarFlagEncoder carEncoder = new CarFlagEncoder();
+    private static EncodingManager encodingManager = new EncodingManager(carEncoder);
+    private static GraphHopper hopper;
+    private static GraphHopperStorage g;
 
-    public static GraphStorage createGraph() {
-//        GraphStorage graph = new GraphBuilder(encodingManager).setLocation(location).setStore(true).create();
-        graph = new GraphBuilder(encodingManager).setLocation(location).setStore(true).create();
+    private static GraphHopperStorage createSquareGraphInstance() {
+
+        Weighting weighting = new FastestWeighting(carEncoder);
+//        GraphHopperStorage g = new GraphHopperStorage(Collections.singletonList(weighting), new RAMDirectory(), encodingManager,
+//                false, new GraphExtension.NoOpExtension()).
+//                create(20);
+         g = new GraphBuilder(encodingManager).setLocation(location).setStore(true).create();
         //Make a weighted edge between two nodes
-        TurnCostExtension turnCostStorage = new TurnCostExtension();
-        NodeAccess nodeAccess = graph.getNodeAccess();
 
-        nodeAccess.setNode(0, 10, 10, 0);
-        nodeAccess.setNode(1, 11, 20, 1);
-        nodeAccess.setNode(2, 12, 12, 0.4);
-//        graph.edge(9, 10, 200, true);
 
-        FlagEncoder encoder = new CarFlagEncoder();
-        EdgeIteratorState edge0 = graph.edge(0, 1, 100, true).setFlags(5);
-//        edge0.setFlags(encoder.setProperties(5, true, true) );
-//        iter2.setWayGeometry(Helper.createPointList3D(1.5, 1, 0, 2, 3, 0));
-        EdgeIteratorState edge1 = graph.edge(0, 2, 150, true).setFlags(2);
-        EdgeIteratorState edge2 = graph.edge(1, 2, 200, false).setFlags(1);
-//        iter1.setWayGeometry(Helper.createPointList3D(3.5, 4.5, 0, 5, 6, 0));
 
-//        turnCostStorage.addTurnInfo(iter1.getEdge(), 0, iter2.getEdge(), 1337);
-//        turnCostStorage.addTurnInfo(iter2.getEdge(), 0, iter1.getEdge(), 666);
-//        turnCostStorage.addTurnInfo(iter1.getEdge(), 1, iter2.getEdge(), 815);
-//
-        edge0.setName("named street1");
-        edge1.setName("named street2");
-        edge2.setName("named street3");
+        //   2---3---4
+        //  /    |    \
+        //  1----8----5
+        //  /    |    /
+        //  0----7---6
+        NodeAccess na = g.getNodeAccess();
+        na.setNode(0, 0.000, 0.000);
+        na.setNode(1, 0.001, 0.000);
+        na.setNode(2, 0.002, 0.000);
+        na.setNode(3, 0.002, 0.001);
+        na.setNode(4, 0.002, 0.002);
+        na.setNode(5, 0.001, 0.002);
+        na.setNode(6, 0.000, 0.002);
+        na.setNode(7, 0.000, 0.001);
+        na.setNode(8, 0.001, 0.001);
 
-        graph.flush();
-        graph.close();
-        return graph;
+        g.edge(0, 1, 100, true).setFlags(5).setName("Street1");
+        g.edge(1, 2, 100, true).setFlags(1);
+        g.edge(2, 3, 100, true).setFlags(1);
+        g.edge(3, 4, 100, true).setFlags(5);
+        g.edge(4, 5, 100, true).setFlags(1);
+        g.edge(5, 6, 100, true).setFlags(5);
+        g.edge(6, 7, 100, true).setFlags(1);
+        g.edge(7, 0, 100, true).setFlags(5);
+
+        g.edge(1, 8, 110, true).setFlags(1);
+        g.edge(3, 8, 110, true).setFlags(5);
+        g.edge(5, 8, 110, true).setFlags(1);
+        g.edge(7, 8, 110, true).setFlags(5);
+//        g.flush();
+//        g.close();
+        return g;
     }
 
 
     public static void main(String[] args) {
-//    public static void main() {
-//        GraphStorage gs = createGraph();
+        GraphHopperStorage graph = createSquareGraphInstance();
+        System.out.println("Nodes: "+graph.getNodes());
+        System.out.println("Edges: "+graph.getAllEdges());
 
-        GraphHopperAPI instance = new GraphHopper()
-                .setEncodingManager(encodingManager)
-                .setGraphHopperLocation(location)
-//                .disableCHShortcuts();
-                .setCHEnable(false);
-
-        GraphHopper hopper = (GraphHopper) instance;
-        //TODO next line comment
-        hopper.setGraphHopperStorage((GraphHopperStorage) createGraph()); // protected because only for testing?
-//        hopper.load(location);
-
-//        index = new LocationIndexTree(graph.getBaseGraph(), new RAMDirectory(location, true));
-//        if (!index.loadExisting())
-//            throw new IllegalStateException("location index cannot be loaded!");
-
-        System.out.println("getNodes: "+hopper.getGraphHopperStorage().getNodes());
-        System.out.println("getAllEdges: "+hopper.getGraphHopperStorage().getAllEdges());
+        hopper = new GraphHopper().
+                setCHEnabled(true).
+                setEncodingManager(encodingManager).
+                setStoreOnFlush(true).
+//                importOrLoad();
+                setGraphHopperLocation(location);
+//                setOSMFile(testOsm3);
+        hopper.getCHFactoryDecorator().setWeightingsAsStrings("fastest");
+        hopper.setGraphHopperStorage(graph);
         AllEdgesIterator iter = hopper.getGraphHopperStorage().getAllEdges();
         while(iter.next()){
             System.out.println("edge name: "+ iter.getName()+", getMaxId: "+iter.getMaxId()+", getEdge: "+iter.getEdge());
             System.out.println("edge getDistance: "+iter.getDistance()+", getFlags: "+iter.getFlags()+"\n");
 
         }
+        graph.flush();
+        graph.close();
 
-//         Load index
-//        LocationIndex index = new LocationIndexTree(graph.getBaseGraph(), new RAMDirectory("graphhopper_folder", true));
-//        if (!index.loadExisting())
-//            throw new IllegalStateException("location index cannot be loaded!");
-
-// calculate path is identical
-//        QueryResult fromQR = index.findClosest(latitudeFrom, longituteFrom, EdgeFilter.ALL_EDGES);
-//        QueryResult toQR = index.findClosest(latitudeTo, longituteTo, EdgeFilter.ALL_EDGES);
-//        QueryGraph queryGraph = new QueryGraph(graph);
-//        queryGraph.lookup(fromQR, toQR);
     }
 }
