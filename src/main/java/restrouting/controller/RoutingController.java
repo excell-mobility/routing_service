a2a0dd63ab71786763c905aee4ea8a74ca194241
Felix Kunde <felix-kunde@gmx.de>
2017-09-21 13:21:07 +0200

    create additional endpoint following web standards

--- src/main/java/restrouting/controller/RoutingController.java
+++ src/main/java/restrouting/controller/RoutingController.java
@@ -20,13 +20,13 @@ import restrouting.component.RoutingService;
 import restrouting.model.RoutingResponse;
 
 @RestController
-@Api(value="/v1/routing")
+@Api(value="routing")
 public class RoutingController {
 	
 	@Autowired
 	RoutingService routingService;
 
-	@RequestMapping(value = "/v1/routing", method = RequestMethod.GET)
+	@RequestMapping(value = "/excell-routing-api/v1/routing", method = RequestMethod.GET)
     @ApiOperation(
     		value = "Find fastest route", 
     		response=RoutingResponse.class,
@@ -48,6 +48,28 @@ public class RoutingController {
 		return routingService.startRouting(startLat, startLon, endLat, endLon);
     }
     
+	@RequestMapping(value = "/api/v1/routing", method = RequestMethod.GET)
+    @ApiOperation(
+    		value = "Find fastest route", 
+    		response=RoutingResponse.class,
+    		produces = "application/json")
+    @ResponseBody
+    public RoutingResponse routing1(
+    		@ApiParam(name="startLat", value="Latitude for start coordinate", defaultValue = "0.0") 
+    		@RequestParam(value="startLat", defaultValue = "0.0") Double startLat,
+    		
+    		@ApiParam(name="startLon", value="Longitude for start coordinate", defaultValue = "0.0") 
+    		@RequestParam(value="startLon", defaultValue = "0.0") Double startLon,
+    		
+    		@ApiParam(name="endLat", value="Latitude for end coordinate", defaultValue = "0.0") 
+    		@RequestParam(value="endLat", defaultValue = "0.0") Double endLat,
+    		
+    		@ApiParam(name="endLon", value="Longitude for end coordinate", defaultValue = "0.0") 
+    		@RequestParam(value="endLon", defaultValue = "0.0") Double endLon) throws RoutingNotFoundException, InputParameterErrorException {
+    	
+		return routingService.startRouting(startLat, startLon, endLat, endLon);
+    }
+	
     @ExceptionHandler(value = InputParameterErrorException.class)
     public BodyBuilder routingParameterError() {
     	return ResponseEntity.status(HttpStatus.BAD_REQUEST);
