48384e8e45e1bddfb19c87363954ed57a898da71
Felix Kunde <felix-kunde@gmx.de>
2018-03-27 11:39:37 +0200

    removed redundant endpoint /api/v1/routing

--- src/main/java/restrouting/controller/RoutingController.java
+++ src/main/java/restrouting/controller/RoutingController.java
@@ -48,28 +48,6 @@ public class RoutingController {
 		return routingService.startRouting(startLat, startLon, endLat, endLon);
     }
 	
-	@RequestMapping(value = "/api/v1/routing", method = RequestMethod.GET)
-    @ApiOperation(
-    		value = "Find fastest route", 
-    		response=RoutingResponse.class,
-    		produces = "application/json")
-    @ResponseBody
-    public RoutingResponse routing1(
-    		@ApiParam(name="startLat", value="Latitude for start coordinate", defaultValue = "51.04") 
-    		@RequestParam(value="startLat", defaultValue = "0.0") Double startLat,
-    		
-    		@ApiParam(name="startLon", value="Longitude for start coordinate", defaultValue = "13.73") 
-    		@RequestParam(value="startLon", defaultValue = "0.0") Double startLon,
-    		
-    		@ApiParam(name="endLat", value="Latitude for end coordinate", defaultValue = "51.029") 
-    		@RequestParam(value="endLat", defaultValue = "0.0") Double endLat,
-    		
-    		@ApiParam(name="endLon", value="Longitude for end coordinate", defaultValue = "13.736")
-    		@RequestParam(value="endLon", defaultValue = "0.0") Double endLon) throws RoutingNotFoundException, InputParameterErrorException {
-    	
-		return routingService.startRouting(startLat, startLon, endLat, endLon);
-    }
-	
     @ExceptionHandler(value = InputParameterErrorException.class)
     public BodyBuilder routingParameterError() {
     	return ResponseEntity.status(HttpStatus.BAD_REQUEST);
